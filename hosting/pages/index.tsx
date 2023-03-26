e5baa7143a7c7081342f292450dd00a8de1137d0
Matthew Nguyen <matthewnguyen1230@gmail.com>
2023-03-25 23:02:48 -0400

    cropped title and added to index

--- hosting/pages/index.tsx
+++ hosting/pages/index.tsx
@@ -5,15 +5,19 @@ import GenerationGraph  from '../components/GenerationGraph'
 export default function IndexPage() {
   
   return (
-    <div className='text-center'>
-      <br /><br /><br /><u><h1 className='text-5xl' >Millennium Power</h1></u><br />
+    <div>
+      <div className="flex justify-center items-center">
+        <br /><br /><img src="title.png" width="528" height="288"/><br />
+      </div>
 
-      <p className='text-3xl'>How would you manage the greatest energy transformation since the invention of the lightbulb?<br /></p>
-      <p className='text-2xl'><br />Millennium Power puts you in charge of managing the power grids of Millenium Town.<br />
-      Do you have what it takes to keep the town happy?</p>
+      <div className='text-center'>
+        <p className='text-3xl'>How would you manage the greatest energy transformation since the invention of the lightbulb?<br /></p>
+        <p className='text-2xl'><br />Millennium Power puts you in charge of managing the power grids of Millenium Town.<br />
+        Do you have what it takes to keep the town happy?</p>
 
-    
-      <Link className='text-3xl button' style={{color: "green"}} href="/play"><b>Play</b></Link>
+      
+        <Link className='text-3xl button' style={{color: "green"}} href="/play"><b>Play</b></Link><br /><br /><br />
+      </div>
     </div>
   )
 }
