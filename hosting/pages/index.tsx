545525ca6b9c4b141bf8e0a80b05072a944f87bd
Matthew Nguyen <matthewnguyen1230@gmail.com>
2023-03-25 21:56:17 -0400

    added index

--- hosting/pages/index.tsx
+++ hosting/pages/index.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link'
 import GenerationGraph  from '../components/GenerationGraph'
 
 export default function IndexPage() {
+  
   return (
-    <div>
-      <h1 className='text-3xl'>Millennium Power</h1>
+    <div className='text-center'>
+      <br /><br /><br /><u><h1 className='text-5xl' >Millennium Power</h1></u><br />
 
-      <p>How would you manage the greatest energy transformation since the invention of the lightbulb?</p>
+      <p className='text-3xl'>How would you manage the greatest energy transformation since the invention of the lightbulb?<br /></p>
+      <p className='text-2xl'><br />Millennium Power puts you in charge of managing the power grids of Millenium Town.<br />
+      Do you have what it takes to keep the town happy?</p>
 
-      <GenerationGraph />
-
-      <Link href="/play">Play</Link>
+      <br /><GenerationGraph /><br />
+      <Link className='text-3xl button' style={{color: "green"}} href="/play"><b>Play</b></Link>
     </div>
   )
 }
